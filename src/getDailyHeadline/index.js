4780b46b1c0fdcf56d4be640b4a9f92814d907ae
home3k <home3k@gmail.com>
2016-07-05 11:16:28 +0800

    kick medium.

--- src/getDailyHeadline/index.js
+++ src/getDailyHeadline/index.js
@@ -5,9 +5,9 @@ import getReddit from './getReddit'
 import getV2 from './getV2'
 
 export default async function getDailyHeadline() {
-  const [HN, reddit, medium, github, v2ex] = await Promise.all([
-    getHN(), getReddit(), getMedium(), getGithub(), getV2()
+  const [HN, reddit, github, v2ex] = await Promise.all([
+    getHN(), getReddit(), getGithub(), getV2()
   ])
 
-  return {HN, reddit, medium, github, v2ex}
+  return {HN, reddit, github, v2ex}
 }
